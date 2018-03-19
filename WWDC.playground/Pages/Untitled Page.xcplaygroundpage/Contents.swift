06ff8ed27f23c699f78c9e828de6ea349974ad5e
Marcel Hagmann <hagmann.marcel@yahoo.com>
2018-03-19 21:53:01 +0100

    - Events hinzugefügt - Today Button

--- WWDC.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ WWDC.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -89,18 +89,58 @@ class EventController {
     var dates: [Date] = []
     
     func createDemonstrationEvents() {
-        events = [Event(title: "Denise Hagmann's Birthday",
-                        startDate: Date(timeIntervalSince1970: 1521471600),
-                        endDate: Date(timeIntervalSince1970: 1521482400),
-                        type: .birthday, isFullTime: false),
+        events = [Event(title: "Denise Hagmann's Birthday (Sister)",
+                        startDate: Date(timeIntervalSince1970: 1523523600),
+                        endDate: Date(timeIntervalSince1970: 1523527200),
+                        type: .birthday, isFullTime: true),
+                  Event(title: "Marcel Hagmann's Birthday (Me)",
+                        startDate: Date(timeIntervalSince1970: 1538384400),
+                        endDate: Date(timeIntervalSince1970: 1538388000),
+                        type: .birthday, isFullTime: true),
+                  Event(title: "WWDC Scholarship Result",
+                        startDate: Date(timeIntervalSince1970: 1524225600),
+                        endDate: Date(timeIntervalSince1970: 1524240000),
+                        type: .important, isFullTime: false),
+                  Event(title: "WWDC Submission",
+                        startDate: Date(timeIntervalSince1970: 1522054800),
+                        endDate: Date(timeIntervalSince1970: 1522558800),
+                        type: .important, isFullTime: false),
                   Event(title: "WWDC Submission Deadline",
-                        startDate: Date(timeIntervalSince1970: 1522605600),
-                        endDate: Date(timeIntervalSince1970: 1522609200),
+                        startDate: Date(timeIntervalSince1970: 1522555200),
+                        endDate: Date(timeIntervalSince1970: 1522558800),
                         type: .important, isFullTime: true),
                   Event(title: "WWDC 2018",
                         startDate: Date(timeIntervalSince1970: 1528099200),
                         endDate: Date(timeIntervalSince1970: 1528480800),
-                        type: .wwdc, isFullTime: false)]
+                        type: .wwdc, isFullTime: false),
+                  Event(title: "New Year 2017",
+                        startDate: Date(timeIntervalSince1970: 1483261200),
+                        endDate: Date(timeIntervalSince1970: 1483264800),
+                        type: .newYear, isFullTime: true),
+                  Event(title: "New Year 2018",
+                        startDate: Date(timeIntervalSince1970: 1514797200),
+                        endDate: Date(timeIntervalSince1970: 1514800800),
+                        type: .newYear, isFullTime: true),
+                  Event(title: "Independence Day",
+                        startDate: Date(timeIntervalSince1970: 1530694800),
+                        endDate: Date(timeIntervalSince1970: 1530698400),
+                        type: .holiday, isFullTime: true),
+                  Event(title: "Martin Luther King Jr. Day",
+                        startDate: Date(timeIntervalSince1970: 1516006800),
+                        endDate: Date(timeIntervalSince1970: 1516010400),
+                        type: .holiday, isFullTime: true),
+                  Event(title: "Memorial Day",
+                        startDate: Date(timeIntervalSince1970: 1527498000),
+                        endDate: Date(timeIntervalSince1970: 1527501600),
+                        type: .holiday, isFullTime: true),
+                  Event(title: "Thanksgiving",
+                        startDate: Date(timeIntervalSince1970: 1542877200),
+                        endDate: Date(timeIntervalSince1970: 1542880800),
+                        type: .holiday, isFullTime: true),
+                  Event(title: "Black Friday",
+                        startDate: Date(timeIntervalSince1970: 1542963600),
+                        endDate: Date(timeIntervalSince1970: 1543003200),
+                        type: .important, isFullTime: false)]
         
         var dateSet: Set<Date> = []
         for event in events {
@@ -161,17 +201,14 @@ class UICalendarDayOverviewTableViewCell: UITableViewCell {
         timeStackView.axis = .vertical
         self.addSubview(timeStackView)
         
-//        startTimeLabel.backgroundColor = UIColor.orange
         startTimeLabel.font = fontTest
         startTimeLabel.textAlignment = .right
-//        endTimeLabel.backgroundColor = UIColor.lightGray
         endTimeLabel.font = fontTest
         endTimeLabel.textAlignment = .right
         
         seperatorView.backgroundColor = UIColor.blue
         self.addSubview(seperatorView)
         
-//        descriptionLabel.backgroundColor = UIColor.green
         self.addSubview(descriptionLabel)
     }
     
@@ -282,13 +319,12 @@ class ViewController: UIViewController {
         calenderDayOverviewTableView.register(UICalendarDayOverviewTableViewCell.self, forCellReuseIdentifier: "cell")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddNewEntryViewController))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Today", style: .plain, target: self, action: nil)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Today", style: .plain, target: self, action: #selector(selectToday))
 
         self.view.backgroundColor = UIColor.lightGray
         
         let today = Date()
-        let eventsss = eventCtrl.getEvents(forDate: today)
-        calenderDayOverviewTableView.data = eventsss
+        calenderDayOverviewTableView.data = eventCtrl.getEvents(forDate: today)
         calenderDayOverviewTableView.selectedDate = today
         
         self.view.addSubview(calenderDayOverviewTableView)
@@ -324,6 +360,16 @@ class ViewController: UIViewController {
         let createEntryVCtrl = CreateEntryViewController()
         self.navigationController?.pushViewController(createEntryVCtrl, animated: true)
     }
+    
+    @objc func selectToday() {
+        let today = Date()
+        calenderDayOverviewTableView.data = eventCtrl.getEvents(forDate: today)
+        calenderDayOverviewTableView.selectedDate = today
+        calenderDayOverviewTableView.reloadData()
+        dataSource = UICalendarViewDataSource()
+        calendarView.delegate = self
+        calendarView.drawCalendar()
+    }
 
 }
 
@@ -339,11 +385,9 @@ extension ViewController: UICalendarViewDelegateProtocol {
     }
 
     func calenderView(_ calendarView: UICalendarView, touchedNextMonthButton: UIButton) {
-        print("Next Button was touched")
     }
 
     func calenderView(_ calendarView: UICalendarView, touchedPreviousMonthButton: UIButton) {
-        print("Previous Button was touched")
     }
 }
 
